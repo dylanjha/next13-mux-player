70528556133e937f7836a9d6d985c09f27d2117c
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2022-10-13 11:34:14 -0700

    create a next13 app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+// ^ this string is what marks this file as a Client Component.
+// Learn more below under "Routing".
+
+export default function Page() {
+  // Your existing page contents here...
+  return <div>hi</div>;
+}
