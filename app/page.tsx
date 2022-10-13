68e6ff348094bdde0d8c9eeefec8347192b2d1ac
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2022-10-13 11:41:44 -0700

    next13 example, (sharp not playing nice with Next 13)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-'use client';
-// ^ this string is what marks this file as a Client Component.
-// Learn more below under "Routing".
+import { experimental_use as use } from 'react';
+import muxBlurHash from "@mux/blurhash";
 
-export default function Page() {
-  // Your existing page contents here...
-  return <div>hi</div>;
+async function fetchData() {
+  const { blurHashBase64 } = await muxBlurHash('FuJSYrK0014ec2LPnm11bzC2MAySAQPqA')
+  return blurHashBase64;
+}
+
+export default function DataView() {
+  const blurhash = use(fetchData());
+
+  return <div>{blurhash}</div>;
 }
